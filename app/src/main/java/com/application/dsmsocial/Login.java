f1d7a491874f14e2cde7b4c8205a0315f17b951b
Max Kindred <maxwell.kindred@drake.edu>
2015-09-26 17:53:56 -0500

    remaking a few things, my laptop and desktop both have difrerent stuff on them, just trying to even out the files between the 2

--- app/src/main/java/com/application/dsmsocial/Login.java
+++ app/src/main/java/com/application/dsmsocial/Login.java
@@ -1,18 +1,16 @@
 package com.application.dsmsocial;
 
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Typeface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import java.sql.SQLException;
 
 public class Login extends AppCompatActivity
 {
-    private static final String DB_NAME = "DSMSocialClubDB.sqlite3";
 
     private ImageButton loginButton;
     private ImageButton guestButton;
@@ -21,25 +19,17 @@ public class Login extends AppCompatActivity
     private EditText inputPassword;
     private TextView loginError;
 
-    private SQLiteDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        ExternalDbOpenHelper dbOpenHelper = new ExternalDbOpenHelper(this,DB_NAME);
-        try {
-            database = dbOpenHelper.openDataBase();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
     }
 
-
-
-
+    public void login(View view)
+    {
+        //Intent intent = new Intent(this, checkout.class);
+    }
 
 }
