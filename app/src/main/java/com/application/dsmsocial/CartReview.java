ed0fc4bb7990e07eb131043ea6126e66f7bbaf64
Jesse <jesse.jeun@drake.edu>
2015-09-26 14:14:35 -0500

    connected button for shopping cart and also connected checkout button to Login page. Made style and format changes theo shopping car and login page to match graphic artists designs.

--- app/src/main/java/com/application/dsmsocial/CartReview.java
+++ app/src/main/java/com/application/dsmsocial/CartReview.java
@@ -1,16 +1,56 @@
 package com.application.dsmsocial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class CartReview extends AppCompatActivity {
+
+    private static final String TAG = "CartTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_review);
+
+        ImageButton mButton = (ImageButton) findViewById(R.id.deleteButton);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "cart button");
+                sendToDelete();
+            }
+        });
+
+        ImageButton mButton2 = (ImageButton) findViewById(R.id.checkoutButton);
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "cart button");
+                sendToCheckout();
+            }
+        });
+    }
+
+
+    public void sendToDelete(){
+        Toast.makeText(CartReview.this, "Item Deleted", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, CartReview.class);
+        startActivity(intent);
+    }
+
+
+    public void sendToCheckout(){
+
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+
     }
 
     @Override
